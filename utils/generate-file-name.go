2610d396e6d059976a6c6fcaa0a829d571b7c2a0
Bagas-AP <bantarino4@gmail.com>
2024-04-29 14:05:50 +0700

    patched file-name algorithm

--- utils/generate-file-name.go
+++ utils/generate-file-name.go
@@ -7,7 +7,8 @@ import (
 
 func GenerateFileName(extension string) string {
 	now := time.Now()
-	fileName := fmt.Sprintf("%02d_%02d_%02d_%02d_%02d_%d%s",
-		now.Second(), now.Minute(), now.Hour(), now.Day(), now.Month(), now.Year(), extension)
+	// Format the time to include nanoseconds
+	fileName := fmt.Sprintf("%09d_%02d_%02d_%02d_%02d_%02d_%d_%s",
+		now.Nanosecond(), now.Second(), now.Minute(), now.Hour(), now.Day(), int(now.Month()), now.Year(), extension)
 	return fileName
 }
